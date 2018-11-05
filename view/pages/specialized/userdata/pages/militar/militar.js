fcc9257edc011184153ca06e9a608017cc18caf5
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-11-05 16:31:37 -0200

    Tabela de unidades militares em planeta

--- view/pages/specialized/userdata/pages/militar/militar.js
+++ view/pages/specialized/userdata/pages/militar/militar.js
@@ -1,7 +1,8 @@
 let $ = require('jquery')
 const observer = require('./../../../../generic/modules/observer')
 const utils = require('./../../../../generic/modules/utils')
-function getMilitar(planetaID)
+const navePrefabs = require('./../../../../../../prefabs/Nave')
+function getMilitar(planetaID, callback = () => {})
 {
     $.ajax({
         url : "militar/frota",
@@ -9,7 +10,7 @@ function getMilitar(planetaID)
         data: {planetaid : planetaID},
         dataType : "JSON",
         success : (unidades) => {
-            console.log(unidades);
+            callback(unidades)
         },
         error : (err) => {
             utils.GerarNotificacao(err.responseText, "error");
@@ -17,7 +18,55 @@ function getMilitar(planetaID)
     })
 }
 
+function encontrarPrefab(nomeTabela)
+{
+    for(let prefab in navePrefabs)
+    {
+        let obj = navePrefabs[prefab];
+        if(nomeTabela == obj.nome_tabela)
+        {
+            return obj;
+        }
+    }
+    return null
+}
+
+function montarHTMLUnidades(unidades)
+{
+    let htmlString = "";
+    for(let unidade in unidades)
+    {
+
+        let prefab = encontrarPrefab(unidade)
+        
+        htmlString += 
+        `
+            <tr>
+                <td>${prefab.nome}</td>
+                <td>${prefab.hp}</td>
+                <td>${prefab.armadura}</td>
+                <td>${prefab.escudo}</td>
+                <td>${prefab.evasao}</td>
+                <td>${prefab.capacidade_transporte}</td>
+                <td>${prefab.velocidade}</td>
+                <td>${prefab.dano}</td>
+                <td>${prefab.quantidade_armas}</td>
+                <td>${unidades[unidade]}</td> <!--total-->
+                <td></td>
+            </tr>
+        `
+    }
+    return htmlString;
+}
 
 observer.Observar('userdata-ready',  () => {
-    getMilitar(window.planeta.id)
+    getMilitar(window.planeta.id, (unidades) => {
+        delete unidades.operacaoID
+        delete unidades.planetaID
+        delete unidades.relatorioID
+        delete unidades.id
+        delete unidades.usuarioID
+        let htmlUnidades = montarHTMLUnidades(unidades);
+        $("#table-unidades tbody").html(htmlUnidades);
+    })
 })
