fb425b59ac098be4d743f829658e26845d75971f
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-11-19 16:52:12 -0200

    Função Combater

--- services/OperacaoController.js
+++ services/OperacaoController.js
@@ -5,6 +5,137 @@ const edificioPrefab = require('./../prefabs/Edificio')
 const navePrefab = require('./../prefabs/Nave')
 const naveBuilder = require('./../prefabs/NaveBuilder')
 const pathFinder = require('./../services/Pathfinder')
+let aleatorio = require("./Aleatorio")
+
+async function Combater(frotaAtacante, frotaDefensora)
+{
+    return new Promise((resolve, reject) => 
+    {
+        function setFrotaObjAlvo(frotaSet, FrotaAlvo)
+        {
+            for(let i = 0; i < frotaSet.length; i++)
+            {
+                let naveOBJ = frotaSet[i]
+                let alvoIndex = aleatorio.GerarIntAleatorio(FrotaAlvo.length - 1 , 0)
+                naveOBJ.alvo = alvoIndex
+            }
+        }
+
+        function executarDanoFrotaObj(frotaDanificadora, frotaVitima)
+        {
+            for(let i = 0; i < frotaDanificadora.length; i++)
+            {
+                let naveOBJ = frotaDanificadora[i]
+                let alvo = frotaVitima[naveOBJ.alvo]
+
+                let quantidadesHits = 0
+                for(let j = 0; j < naveOBJ.nave.quantidade_armas; j++)
+                {
+                    let hitValor = aleatorio.GerarIntAleatorio(100, 0)
+                    if(hitValor <= alvo.nave.evasao)
+                    {
+                        quantidadesHits++
+                    }
+                }
+                
+                let dano = (naveOBJ.nave.dano - naveOBJ.nave.armadura) * quantidadesHits
+
+                alvo.nave.hp -= dano
+                
+            }
+        }
+
+        function getSobreviventesFrotaObj(frotaObj)
+        {
+            let sobrevivientes = []
+            for(let i = 0; i < frotaObj.length; i++)
+            {
+                if(frotaObj[i].nave.hp > 0)
+                {
+                    sobrevivientes.push(frotaObj[i])
+                }
+            }
+            return sobrevivientes
+        }
+
+        let objFrotaAtacante = []
+        for(let chave in frotaAtacante)
+        {
+            
+            let nave = naveBuilder.getNavePorNomeTabela(chave)
+            
+            for(let i = 0; i < frotaAtacante[chave]; i++)
+            {
+                let clone = Object.assign( Object.create( Object.getPrototypeOf(nave)), nave)
+                objFrotaAtacante.push({nave : clone, alvo : null})
+            }
+            
+        }
+
+        let objFrotaDefensora = []
+        for(let chave in frotaDefensora)
+        {
+            let nave = naveBuilder.getNavePorNomeTabela(chave)
+            
+            for(let i = 0; i < frotaDefensora[chave]; i++)
+            {
+                let clone = Object.assign( Object.create( Object.getPrototypeOf(nave)), nave)
+                objFrotaDefensora.push({nave : clone, alvo : null})
+            }
+            
+        }
+
+
+        
+        let intervalo = setInterval(() => 
+        {
+            //for(let naveOBJ of objFrotaAtacante)
+            setFrotaObjAlvo(objFrotaAtacante, objFrotaDefensora)
+            setFrotaObjAlvo(objFrotaDefensora, objFrotaAtacante)
+            
+            executarDanoFrotaObj(objFrotaAtacante, objFrotaDefensora)
+            console.log("?????????????????????????")
+            executarDanoFrotaObj(objFrotaDefensora, objFrotaAtacante)
+
+            objFrotaAtacante = getSobreviventesFrotaObj(objFrotaAtacante)
+            objFrotaDefensora = getSobreviventesFrotaObj(objFrotaDefensora)
+
+            if(objFrotaAtacante.length == 0 || objFrotaDefensora.length == 0)
+            {
+
+                for(let chave in frotaAtacante)
+                {
+                    frotaAtacante[chave] = 0                
+                }
+
+                for(let i = 0; i < objFrotaAtacante.length; i++)
+                {
+                    let nome_tabela = objFrotaAtacante[i].nave.nome_tabela
+                    frotaAtacante[nome_tabela]++
+                }
+
+                for(let chave in frotaDefensora)
+                {
+                    frotaDefensora[chave] = 0                
+                }
+
+                for(let i = 0; i < objFrotaDefensora.length; i++)
+                {
+                    let nome_tabela = objFrotaDefensora[i].nave.nome_tabela
+                    frotaDefensora[nome_tabela]++
+                }
+                clearInterval(intervalo)
+                resolve({atacante : frotaAtacante, defensora : frotaDefensora})                
+            }
+
+        }, 1000)   
+
+    })
+}
+
+
+
+
 
 
 function MoverFrota(operacaoID, setorOrigem, setorDestino, frota)
